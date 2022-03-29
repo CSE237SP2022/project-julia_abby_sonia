9fe7fd4d10cd4a99ea60a5d10c39b321444d2f08
juliachandonnet <54594387+juliachandonnet@users.noreply.github.com>
2022-03-29 00:41:41 -0500

    adding user tests

---
+++ src/test/RosterTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import RosterAndStudent.Roster;
+import RosterAndStudent.Student;
+
+class RosterTest {
+
+	@Test
+	void testRosterSizeOne() {
+		ArrayList <Student> CSE237list = new ArrayList<Student>();
+		Roster CSE237 = new Roster(CSE237list);
+		Student henry = new Student("henry", 33.0);
+		CSE237list.add(henry);
+		int classSize = CSE237.getSize();
+		assertEquals(1, classSize);
+	}
+	
+	@Test
+	void testRosterSizeTwo() {
+		ArrayList <Student> CSE237list = new ArrayList<Student>();
+		Roster CSE237 = new Roster(CSE237list);
+		Student ray = new Student("ray", 44.50);
+		CSE237list.add(ray);
+		Student sheila = new Student("sheila", 36.72);
+		CSE237list.add(sheila);
+		int classSize = CSE237.getSize();
+		assertEquals(2, classSize);
+	}
+
+}
---
+++ src/test/StudentTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import RosterAndStudent.Student;
+
+class StudentTest {
+
+	@Test
+	void testName() {
+		Student abby = new Student("Abby", 99.0);
+		String name = abby.getName();
+		assertEquals("Abby", name);
+	}
+	
+	@Test
+	void testGrade() {
+		Student julia = new Student ("Julia", 20.1);
+		double grade = julia.getGrade();
+		assertEquals(20.1, grade);
+		
+	}
+
+}
